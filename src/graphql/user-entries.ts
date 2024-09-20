8733695955e8972c6be90eb5fe69fb5bbf1ebd1f
Alejo Mendoza <alejandro@skyhitzmusic.com>
2024-09-20 10:54:58 -0500

    fix empty collection

--- src/graphql/user-entries.ts
+++ src/graphql/user-entries.ts
@@ -6,7 +6,6 @@ export const userEntriesResolver = async (root: any, { userId }: any, { env }: C
 	const algolia = new AlgoliaClient(env);
 	try {
 		const entries = await algolia.getCollection(userId);
-		console.log('collection:', entries);
 		return await algolia.getMultiEntries(entries.map((entry) => entry.entryId));
 	} catch (_) {
 		throw new GraphQLError('Couldn not fetch user entries');
