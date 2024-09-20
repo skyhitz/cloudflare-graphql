014271d880cdaafd3c982d0bb5ef4c98d4694474
Alejo Mendoza <alejandro@skyhitzmusic.com>
2024-09-20 10:50:29 -0500

    debug user entries

--- src/graphql/user-entries.ts
+++ src/graphql/user-entries.ts
@@ -5,9 +5,8 @@ import { Context } from '../util/types';
 export const userEntriesResolver = async (root: any, { userId }: any, { env }: Context) => {
 	const algolia = new AlgoliaClient(env);
 	try {
-		const user = await algolia.getUser(userId);
-
-		const entries = await algolia.getCollection(user.id);
+		const entries = await algolia.getCollection(userId);
+		console.log('collection:', entries);
 		return await algolia.getMultiEntries(entries.map((entry) => entry.entryId));
 	} catch (_) {
 		throw new GraphQLError('Couldn not fetch user entries');
